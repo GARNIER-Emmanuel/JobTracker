164892dc910f46f4e7d7320b241569c3a2f6b293
GARNIER-Emmanuel <manu.boidun@gmail.com>
2026-06-01 09:50:11 +0200

    feat(router): configure standalone layout and lazy loaded jobs route

--- front-jobtracker/src/app/app.routes.ts
+++ front-jobtracker/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'jobs',
+        pathMatch: 'full'
+    },
+    {
+        path: 'jobs',
+        loadComponent: () => import('./components/jobs/jobs').then(m => m.Jobs)
+    }
+];
